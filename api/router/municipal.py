e6a4d74ec0beb9a4d43464dae5afaa89821aef9e
Joyce Silva <joycepereirasbs@gmail.com>
2026-04-15 21:48:05 -0300

    feat: RF[04]  criação de endpoint para filtrar imoveis com interseção e criação de endpoint

--- api/router/municipal.py
+++ api/router/municipal.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from models.database import SessionConnection
 from api.utils.basic_response import BasicResponse
-from api.schemas.municipal import ResponseMunicipal
-from api.router.controller.municipal_controller import MunicipalHandler
+from api.schemas.municipal import ResponseMunicipal, GeoJSONGeometry, ResponseImovelRuralIntersection
+from api.router.controller.municipal_controller import MunicipalHandler, IntersectionHandler
 
 router = APIRouter(
     tags=["Municipal"],
@@ -18,3 +18,10 @@ async def get_all_municipal_data(session: AsyncSession = Depends(SessionConnecti
 @router.get("/{municipio_id}")
 async def get_municipal_data_by_id(municipio_id: int, session: AsyncSession = Depends(SessionConnection.session)) -> BasicResponse[list[ResponseMunicipal]]:
     return await MunicipalHandler(session, municipio_id).execute()
+
+@router.post("/intersections")
+async def get_intersecting_imoveis(
+    geojson_geometry: GeoJSONGeometry,
+    session: AsyncSession = Depends(SessionConnection.session)
+) -> BasicResponse[list[ResponseImovelRuralIntersection]]:
+    return await IntersectionHandler(session, geojson_geometry).execute()
